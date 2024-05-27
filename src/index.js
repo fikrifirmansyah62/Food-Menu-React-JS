3182583d292ee8e05c4f30356efd391370591c01
M Fikri Firmansyah <fikrifirmansyahm62@gmail.com>
2024-05-27 15:34:04 +0700

    Menambahkan Eksternal Styling Index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./index.css";
+
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -12,20 +14,38 @@ function App() {
 }
 
 function Header() {
-  return <h1>Warteg Jaya Abadi</h1>;
+  return (
+    <header className="header">
+      <h1>Warteg Jaya Abadi</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Menu items</h2>
       <Food />
-    </div>
+    </main>
   );
 }
 
 function Footer() {
-  return <footer>{new Date().getFullYear()} Warteg Jaya Abadi</footer>;
+  const hour = new Date().getHours();
+  const jamBuka = 8;
+  const jamTutup = 20;
+
+  if (hour < jamBuka || hour > jamTutup) {
+    alert("Warteg Jaya Abadi Tutup");
+  } else {
+    alert("Warteg Jaya Abadi Buka");
+  }
+
+  return (
+    <footer className="footer">
+      {new Date().getFullYear()} Warteg Jaya Abadi || Jam Buka {jamBuka} - Jam Tutup {jamTutup}{" "}
+    </footer>
+  );
 }
 
 function Food() {
